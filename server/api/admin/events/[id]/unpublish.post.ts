9a88b24bded474b84947f16ebc3d2f0bd4bf6d69
Le Ngo Duc Manh <myt@nnsvn.me>
2026-05-08 22:43:01 +0700

    refactor: normalize event unpublish response

--- server/api/admin/events/[id]/unpublish.post.ts
+++ server/api/admin/events/[id]/unpublish.post.ts
@@ -1,12 +1,13 @@
 import eventService from '~~/server/utils/database/event'
-import { success } from '~~/server/utils/apiResponse'
+import { apiError, success } from '~~/server/utils/apiResponse'
 
 export default defineEventHandler(async (event) => {
   const eventId = Number(getRouterParam(event, 'id'))
   if (Number.isNaN(eventId)) {
-    throw createError({ statusCode: 400, statusMessage: 'Bad Request. Event ID is invalid.' })
+    throw apiError({ status: 400, statusText: 'Bad Request', code: 'INVALID_EVENT_ID', message: 'Event ID is invalid.' })
   }
 
   const unpublished = await eventService.unpublish(eventId)
-  return success(unpublished)
+  const response: Awaited<ReturnType<typeof eventService.unpublish>> = unpublished
+  return success(response)
 })
