304b75ab9deb137b56f9ea521f5d4586197134aa
hongphuc-git <hongphucnguyen.edu@gmail.com>
2026-05-17 09:01:26 +0700

    chore: keep demo seed local

--- server/api/admin/tasks/seed-demo-ticketing.post.ts
+++
@@ -1,7 +0,0 @@
-import { seedDemoTicketingData } from '~~/server/tasks/db/demo-ticketing'
-import { success } from '~~/server/utils/apiResponse'
-
-export default defineEventHandler(async () => {
-  const response = await seedDemoTicketingData()
-  return success(response)
-})
